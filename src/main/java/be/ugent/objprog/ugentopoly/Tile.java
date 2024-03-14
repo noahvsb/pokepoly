a4b21f674c13e70b771de3b7fe771bd98116c67f
NoahVS <noah.vansteenbrugge@ugent.be>
2024-03-14 14:40:36 +0100

    commit test

--- src/main/java/be/ugent/objprog/ugentopoly/Tile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Tile.java
@@ -5,11 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.Border;
-import javafx.scene.paint.Paint;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
@@ -18,6 +14,8 @@ import java.util.Objects;
 
 public class Tile {
 
+    private HBox tileHBox;
+    private VBox tileVBox;
     private Button button;
     private ImageView imageView;
 
@@ -30,10 +28,31 @@ public class Tile {
     public Tile(String tileName, String imageName, int width, int height) {
         this.tileName = new Text(tileName);
         this.tileName.setFont(new Font(11));
+        this.tileName.setTextAlignment(TextAlignment.CENTER);
 
         this.imageName = imageName;
         this.width = width;
         this.height = height;
+
+        tileHBox = new HBox(this.tileName);
+        tileHBox.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        tileHBox.setRotate(0);
+        tileHBox.setPrefSize(this.width, this.height);
+        tileHBox.setAlignment(Pos.CENTER);
+        tileHBox.setSpacing(5);
+        tileHBox.setOnMouseClicked(e -> showInfo());
+        tileHBox.setStyle("-fx-border-color: black; -fx-border-width: 1.5; -fx-background-color: lightgreen");
+
+        tileVBox = new VBox(this.tileName);
+        tileVBox.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        tileVBox.setRotate(0);
+        tileVBox.setPrefSize(this.width, this.height);
+        tileVBox.setAlignment(Pos.CENTER);
+        tileVBox.setSpacing(5);
+        tileVBox.setOnMouseClicked(e -> showInfo());
+        tileVBox.setStyle("-fx-border-color: black; -fx-border-width: 1.5; -fx-background-color: lightgreen");
+
+
 
         if (this.imageName != null) {
             imageView = new ImageView();
@@ -41,16 +60,31 @@ public class Tile {
             imageView.setFitWidth(this.width / 3);
             imageView.setFitHeight(this.height / 3);
 
+            tileHBox.getChildren().add(imageView);
+
+            tileVBox.getChildren().add(imageView);
+
             button = new Button(this.tileName.getText(), imageView);
         } else {
             button = new Button(this.tileName.getText());
         }
 
-        button.setAlignment(Pos.CENTER);
-        button.setTextAlignment(TextAlignment.CENTER);
         button.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
         button.setRotate(0);
         button.setPrefSize(this.width, this.height);
+
+
+    }
+
+    private void showInfo() {
+        System.out.println("info");
+    }
+
+    public HBox getHBox() {
+        return tileHBox;
+    }
+    public VBox getVBox() {
+        return tileVBox;
     }
 
     public Button getButton() {
--- src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
@@ -22,84 +22,7 @@ public class Ugentopoly extends Application {
     public void start(Stage stage) {
 
         // spelbord
-        ArrayList<Tile> leftTiles = new ArrayList<>();
-        leftTiles.add(new Tile("tile 9", null, 130, 65));
-        leftTiles.add(new Tile("tile 8", null, 130, 65));
-        leftTiles.add(new Tile("tile 7", null, 130, 65));
-        leftTiles.add(new Tile("tile 6", null, 130, 65));
-        leftTiles.add(new Tile("tile 5", null, 130, 65));
-        leftTiles.add(new Tile("tile 4", null, 130, 65));
-        leftTiles.add(new Tile("tile 3", null, 130, 65));
-        leftTiles.add(new Tile("tile 2", null, 130, 65));
-        leftTiles.add(new Tile("tile 1", null, 130, 65));
-
-        VBox left = new VBox();
-        for (Tile t : leftTiles)
-            left.getChildren().add(t.getButton());
-
-        ArrayList<Tile> topTiles = new ArrayList<>();
-        topTiles.add(new Tile("overpoort", "jail", 130, 130));
-        topTiles.add(new Tile("tile 11", null, 65, 130));
-        topTiles.add(new Tile("tile 12", null, 65, 130));
-        topTiles.add(new Tile("tile 13", null, 65, 130));
-        topTiles.add(new Tile("tile 14", null, 65, 130));
-        topTiles.add(new Tile("tile 15", null, 65, 130));
-        topTiles.add(new Tile("tile 16", null, 65, 130));
-        topTiles.add(new Tile("tile 17", null, 65, 130));
-        topTiles.add(new Tile("tile 18", null, 65, 130));
-        topTiles.add(new Tile("tile 19", null, 65, 130));
-        topTiles.add(new Tile("gratis\nparkeren", "free_parking", 130, 130));
-
-        HBox top = new HBox();
-        for (Tile t : topTiles)
-            top.getChildren().add(t.getButton());
-
-        ArrayList<Tile> rightTiles = new ArrayList<>();
-        rightTiles.add(new Tile("tile 21", null, 130, 65));
-        rightTiles.add(new Tile("tile 22", null, 130, 65));
-        rightTiles.add(new Tile("tile 23", null, 130, 65));
-        rightTiles.add(new Tile("tile 24", null, 130, 65));
-        rightTiles.add(new Tile("tile 25", null, 130, 65));
-        rightTiles.add(new Tile("tile 26", null, 130, 65));
-        rightTiles.add(new Tile("tile 27", null, 130, 65));
-        rightTiles.add(new Tile("tile 28", null, 130, 65));
-        rightTiles.add(new Tile("tile 29", null, 130, 65));
-
-        VBox right = new VBox();
-        for (Tile t : rightTiles)
-            right.getChildren().add(t.getButton());
-
-        ArrayList<Tile> bottomTiles = new ArrayList<>();
-        bottomTiles.add(new Tile("start", "start", 130, 130));
-        bottomTiles.add(new Tile("tile 39", null, 65, 130));
-        bottomTiles.add(new Tile("tile 38", null, 65, 130));
-        bottomTiles.add(new Tile("tile 37", null, 65, 130));
-        bottomTiles.add(new Tile("tile 36", null, 65, 130));
-        bottomTiles.add(new Tile("tile 35", null, 65, 130));
-        bottomTiles.add(new Tile("tile 34", null, 65, 130));
-        bottomTiles.add(new Tile("tile 33", null, 65, 130));
-        bottomTiles.add(new Tile("tile 32", null, 65, 130));
-        bottomTiles.add(new Tile("tile 31", null, 65, 130));
-        bottomTiles.add(new Tile("go to\noverpoort", "go_to_jail", 130, 130));
-
-        HBox bottom = new HBox();
-        for (Tile t : bottomTiles)
-            bottom.getChildren().add(t.getButton());
-
-        ImageView logo = new ImageView();
-        logo.setImage(new Image(Objects.requireNonNull(getClass().getResource("assets/logo.png")).toExternalForm()));
-        logo.setFitWidth(400);
-        logo.setFitHeight(75);
-        logo.setRotate(45);
-
-        BorderPane board = new BorderPane();
-        board.setBottom(bottom);
-        board.setLeft(left);
-        board.setTop(top);
-        board.setRight(right);
-        board.setCenter(logo);
-
-        Scene spelbordScene = new Scene(board, 845, 845);
+        Scene spelbordScene = new Scene(new Bord().getMainPane(), 845, 845);
 
 
         //dobbelstenen
@@ -108,7 +31,7 @@ public class Ugentopoly extends Application {
         Button btn = new Button();
         btn.setText("ROL");
         btn.setFont(new Font(40));
-        btn.setOnAction(event -> dice.roll(t -> System.out.println("Resultaat: ")));
+        btn.setOnAction(event -> dice.roll(t -> System.out.println("Resultaat: " + dice.getLastRoll())));
         btn.setPrefSize(200, 100);
 
         StackPane root = new StackPane();
