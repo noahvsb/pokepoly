8c59d997779362a86752fc365c1727fe96c758b8
NoahVS <noah.vansteenbrugge@ugent.be>
2024-03-20 17:08:28 +0100

    commit test

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/ChanceTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/ChanceTile.java
@@ -1,19 +1,9 @@
 package be.ugent.objprog.ugentopoly.tiles;
 
-import be.ugent.objprog.ugentopoly.Bord;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Properties;
 
 public class ChanceTile extends Tile {
 
--- src/main/java/be/ugent/objprog/ugentopoly/tiles/StreetTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/StreetTile.java
@@ -1,17 +1,12 @@
 package be.ugent.objprog.ugentopoly.tiles;
 
-import be.ugent.objprog.ugentopoly.Bord;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 import java.io.IOException;
-import java.util.Properties;
 
 public class StreetTile extends Tile {
 
