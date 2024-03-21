362d4384f2c7941df78b70ef97ebf94e8deb9259
tibvdm <tibovandemoortele@hotmail.com>
2024-03-21 14:25:05 +0100

    open module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
-module be.ugent.objprog.ugentopoly {
+open module be.ugent.objprog.ugentopoly {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.jdom2;
     requires be.ugent.objprog.dice;
-    requires jdom;
-
-    opens be.ugent.objprog.ugentopoly to javafx.fxml;
-    exports be.ugent.objprog.ugentopoly;
-    exports be.ugent.objprog.ugentopoly.tiles;
-    opens be.ugent.objprog.ugentopoly.tiles to javafx.fxml;
 }
