1181d2cb41005ec67e7c78488a2fee5df696cf37
NoahVS <noah.vansteenbrugge@ugent.be>
2024-05-02 21:44:55 +0200

    commit test

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/card/Deck.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/card/Deck.java
@@ -8,38 +8,41 @@ import java.util.Collections;
 public class Deck extends ArrayList<Element> {
     private int type; // 1 = chance, -1 = chest
     private int cardCounter;
-    private Element getOutOfJailCard;
+    private int jailCardPos;
+    private boolean hasJailCard;
 
     public Deck(Element deckElement, int type) {
         addAll(deckElement.getChildren());
+        Collections.shuffle(this);
 
         this.type = type;
         cardCounter = -1;
-        getOutOfJailCard = deckElement.getChildren().getFirst();
-        Collections.shuffle(this);
+
+        jailCardPos = 0;
+        for (Element e : this) {
+            if (e.getAttributeValue("type").equals("JAIL"))
+                break;
+            jailCardPos++;
+        }
+        hasJailCard = true;
     }
 
     public Element getTopCard() {
         cardCounter++;
         if (cardCounter == size())
             cardCounter = 0;
+        if (!hasJailCard && cardCounter == jailCardPos)
+            cardCounter += jailCardPos == size() - 1 ? -cardCounter : 1;
+
         return get(cardCounter);
     }
 
-    public boolean removeGetOutOfJailCard() {
-        if (getFirst().equals(getOutOfJailCard)) {
-            removeFirst();
-            return true;
-        }
-        return false;
+    public void removeGetOutOfJailCard() {
+        hasJailCard = false;
     }
 
-    public boolean addGetOutOfJailCard() {
-        if (!contains(getOutOfJailCard)) {
-            add(getOutOfJailCard);
-            return true;
-        }
-        return false;
+    public void addGetOutOfJailCard() {
+        hasJailCard = true;
     }
 
     public int getType() {
